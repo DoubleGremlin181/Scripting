ebdfab7e47893b36b14c5653d3f6511c9323196c
DoubleGremlin181 <khukmani@gmail.com>
2016-10-16 00:01:31 +0530

    Update manga.py

--- manga.py
+++ manga.py
@@ -11,60 +11,89 @@ from bs4 import BeautifulSoup
 from PIL import Image
 from io import BytesIO
 
-base_url = "http://www.mangaeden.com/en/en-manga/"
+def extractData(url):
+    page = requests.get(url).content
+    soup = BeautifulSoup(page, "lxml")
+    return soup
 
-n = raw_input("Enter the name of the manga\n> ")
-name = n.lower().strip().replace(' ','-')
-url = base_url + name + "/"
-page = requests.get(url).content
-soup1 = BeautifulSoup(page, "lxml")
-if soup1.title.string == "\n404 NOT FOUND - Manga Eden\n":
-	print "404 NOT FOUND"
-	exit()
-mangaDescription = soup1.find("h2",{"id":"mangaDescription"}).string
-print "\nManga Description:\n" + mangaDescription
-ch = raw_input("Do you want to download a chapter?\n> ")
-if ch == "y" or ch == "yes":
-	if not os.path.exists(n):
-		os.mkdir( n, 0755 )
-	chapter = raw_input("Enter the chapter number\n> ")
-	chapter_path = n + "/" + chapter
-	if not os.path.exists(chapter_path):
-		os.mkdir(chapter_path , 0755 )
-	chapter_url = url + chapter +"/"
-	page1_data  = requests.get(chapter_url).content
-	soup2 = BeautifulSoup(page1_data, "lxml")
-	last_page = int(soup2.find_all("option")[-1].get_text())
-	for i in range(1,last_page+1):
-		if glob.glob(chapter_path + "/" + str(i) + "*") and i != 1:
-			print ("Progress: Page "+str(i)+" of "+str(last_page))
-			continue
-		page_url = chapter_url + str(i)
-		page_data = requests.get(page_url).content
-		soup3 = BeautifulSoup(page_data,"lxml")
-		img_block = soup3.find("a",{"id":"nextA"})
-		img_tag = img_block.find("img")["src"]
-		img_url = "https:" + img_tag
-		ext = img_url[img_url.rfind("."):]
-		if i == 1:
-			ext1 = ext
-		img_name = chapter_path + "/" + str(i) + ext
-		img_data = requests.get(img_url)
-		img = Image.open(BytesIO(img_data.content))
-		img.save(img_name)
-		print ("Progress: Page "+str(i)+" of "+str(last_page))
-	print "Download complete"
-	ch = raw_input("\nDo you want to open the chapter? yes or no\n> ")
-	if ch == "y" or ch == "yes":
-		print "Opening chapter"
-		subprocess.call(["eog", chapter_path + "/1" + ext1])
-	elif ch == "n" or ch == "no":
-		exit()
-	else:
-		print "Invalid input"
-		
-elif ch == "n" or ch == "no":
-	exit()
-	
-else:
-	print "Invalid input"
+def mangaDescription(desc_page):
+    mangaDescription = desc_page.find("h2",{"id":"mangaDescription"}).string
+    print "\nManga Description:\n" + mangaDescription
+    pass
+
+def makeFolder(path):
+    if not os.path.exists(path):
+		os.mkdir( path, 0755 )
+    pass
+
+def imageURL(page):
+    page_url = chapter_url + str(page)
+    page_data = extractData(page_url)
+    img_block = page_data.find("a",{"id":"nextA"})
+    img_tag = img_block.find("img")["src"]
+    return "https:" + img_tag
+
+def downloadImage(page_no):
+    if glob.glob(chapter_path + "/" + str(page_no) + "*") and page_no != 1:
+        print ("Progress: Page "+str(page_no)+" of "+str(last_page))
+        return
+    img_url = imageURL(page_no)
+    ext = img_url[img_url.rfind("."):]
+    img_name = chapter_path + "/" + str(page_no) + ext
+    img_data = requests.get(img_url)
+    img = Image.open(BytesIO(img_data.content))
+    img.save(img_name)
+    print ("Progress: Page "+str(page_no)+" of "+str(last_page))
+    pass
+
+def downloadChapter():
+    global chapter_path, chapter_url, last_page
+
+    makeFolder(n)
+    chapter = raw_input("Enter the chapter number\n> ")
+    chapter_path = n + "/" + chapter
+    makeFolder(chapter_path)
+    chapter_url = url + chapter +"/"
+    page1_data = extractData(chapter_url)
+    last_page = int(page1_data.find_all("option")[-1].get_text())
+    for i in range(1, last_page + 1):
+        downloadImage(i)
+    print "Download complete"
+    open()
+    pass
+
+def open():
+    ch = raw_input("\nDo you want to open the chapter? yes or no\n> ")
+    if ch == "y" or ch == "yes":
+        print "Opening chapter"
+        subprocess.call(["eog", glob.glob(chapter_path + "/1.*")[0]])
+    elif ch == "n" or ch == "no":
+        exit()
+    else:
+        print "Invalid input"
+    pass
+
+def main():
+    global n, url
+
+    base_url = "http://www.mangaeden.com/en/en-manga/"
+    n = raw_input("Enter the name of the manga\n> ")
+    name = n.lower().strip().replace(' ','-')
+    url = base_url + name + "/"
+    desc = extractData(url)
+    if desc.title.string == "\n404 NOT FOUND - Manga Eden\n":
+    	print "404 NOT FOUND"
+    	exit()
+    mangaDescription(desc)
+    ch = raw_input("Do you want to download a chapter?\n> ")
+    if ch == "y" or ch == "yes":
+        downloadChapter()
+    elif ch == "n" or ch == "no":
+    	exit()
+    else:
+    	print "Invalid input"
+    pass
+
+if __name__ == '__main__':
+    main()
+
